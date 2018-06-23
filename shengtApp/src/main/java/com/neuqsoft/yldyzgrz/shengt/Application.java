1aff2392f90557c91d30ca12e79bc1da6d447f9c
yourname <maw@neuqsoft.com>
2018-06-23 12:00:25 +0800

    compile

---
+++ shengtApp/src/main/java/com/neuqsoft/yldyzgrz/shengt/Application.java
@@ -0,0 +1,13 @@
+package com.neuqsoft.yldyzgrz.shengt;
+
+import com.basego.cool.core.base.BaseApp;
+
+/**
+ * 项目名称：IeptProject
+ * 类描述：
+ * 创建人：maw@neuqsoft.com
+ * 创建时间： 2018/6/23 11:59
+ * 修改备注
+ */
+public class Application extends BaseApp {
+}
